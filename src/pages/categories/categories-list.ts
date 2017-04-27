05f5270b6da91abef5454532ef6274585242cc41
valentina.marotto <valentina.marotto@gmail.com>
2017-04-27 17:31:44 +0200

    update translation key, splashscreen, etc

--- src/pages/categories/categories-list.ts
+++ src/pages/categories/categories-list.ts
@@ -3,6 +3,7 @@ import {NavController, NavParams} from 'ionic-angular';
 //import {ItemDetailsPage} from '../item-details/item-details';
 //import {PoiListPage} from '../poi/poi-list';
 import {PoiRoot} from '../poi/poi';
+import { TranslateService } from 'ng2-translate';
 
 
 @Component({
@@ -14,15 +15,16 @@ export class CategoriesListPage {
   //items: Array<{title: string, note: string, icon: string}>;
   items: Array<{title: string}>;
 
-public categories : any = [ {title : "Monumenti", path : "monuments"},
-                            {title : "Musei", path : "museums"},
-                            {title : "Archeologia", path : "archeoSites"},
-                            {title : "Giardini", path : "gardens"}];
+public categories : any = [ {title : this.translate.instant('PAGE_MONUMENTS'), path : "monuments"},
+                            {title : this.translate.instant('PAGE_MUSEUMS'), path : "museums"},
+                            {title : this.translate.instant('PAGE_ARCHEOLOGY'), path : "archeoSites"},
+                            {title : this.translate.instant('PAGE_GARDENS'), path : "gardens"}];
+
 
   // , {title : "Eventi", path : "events"}, {title : "Ristoranti", path : "restaurants"}
 
 
-  constructor(public navCtrl: NavController, navParams: NavParams) {
+  constructor(public navCtrl: NavController, navParams: NavParams,  public translate: TranslateService) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
     this.items = [];
