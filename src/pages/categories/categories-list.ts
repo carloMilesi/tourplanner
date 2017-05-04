9508dd3fa28cb24547816c3f939b502a5f4d7838
valentina.marotto <valentina.marotto@gmail.com>
2017-05-04 15:15:16 +0200

    ion-card with images

--- src/pages/categories/categories-list.ts
+++ src/pages/categories/categories-list.ts
@@ -18,10 +18,12 @@ export class CategoriesListPage {
 public categories : any = [ {title : this.translate.instant('PAGE_MONUMENTS'), path : "monuments"},
                             {title : this.translate.instant('PAGE_MUSEUMS'), path : "museums"},
                             {title : this.translate.instant('PAGE_ARCHEOLOGY'), path : "archeoSites"},
-                            {title : this.translate.instant('PAGE_GARDENS'), path : "gardens"}];
+                            {title : this.translate.instant('PAGE_GARDENS'), path : "gardens"} ];//,
+                           /* {title : this.translate.instant('PAGE_EVENTS'), path : "events"},
+                            {title : this.translate.instant('PAGE_RESTAURANTS'), path : "restaurants"}*/
 
 
-  // , {title : "Eventi", path : "events"}, {title : "Ristoranti", path : "restaurants"}
+
 
 
   constructor(public navCtrl: NavController, navParams: NavParams,  public translate: TranslateService) {
