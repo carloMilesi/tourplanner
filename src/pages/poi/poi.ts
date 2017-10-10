c0f37a496007bf4f9dfc76489212f0b9d34e122b
linux <crl.milesi@gmail.com>
2017-10-10 15:50:03 +0200

    text

--- src/pages/poi/poi.ts
+++ src/pages/poi/poi.ts
@@ -26,8 +26,15 @@ export class PoiRoot {
     console.log("Poi root constructor")
     console.log("item param: " + JSON.stringify(params.get('path')))
     
-    this.poiTitle  = params.get("path");
+    console.log("Pathway root constructor")
+    console.log("item param: " + JSON.stringify(params.get('pathway')))
+    
 
+    if (params.get('path'))
+      this.poiTitle  = params.get("path");
+    else if (params.get("pathway"))
+        this.poiTitle  = params.get("pathway").title;     
+  
     this.poiParams = {pathway : params.get("pathway"), path : params.get("path") }; // i parametri sono passati nel template
 
     this.tab1 = PoiListPage;
